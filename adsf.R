80f947658c61d0140324c373a0d698f6e5f808b8
Suman <sumanpaikcs@gmail.com>
2021-11-13 13:08:57 +0530

    R concepts

---
+++ adsf.R
@@ -0,0 +1,49 @@
+# This is the 1st case:
+a="pen"
+a
+b = 34
+b
+
+# This is the 2nd case
+m1 <- matrix(c(1,2,3,4,5,6))
+m1
+
+# This is the 3rd case
+num1 <- 34
+num2 <- 23
+num1 + num2
+class(num1)
+
+# THis is the 4th case
+char = "Hello R"
+class(char)
+
+# This is the 5th case
+char1 = "R programming language"
+char1
+
+# This is the 6th case
+# log1 = T     
+log1 = TRUE
+log1
+class(log1)
+
+# This is the 7th case
+complex = 5+34i
+complex
+class(complex)
+
+# This is the 8th case
+a <- 4
+a
+b <- 3
+b
+3 -> c
+c
+
+# This is the 8th case
+
+
+
+
+
